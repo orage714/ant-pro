be7921bd5814ab1b578bbfa45e74ab1112169ecc
smallorange <957644070@qq.com>
2019-10-11 22:44:18 +0800

    feat:render props 抽离组件公共逻辑

---
+++ src/pages/Hoc/components/RenderProp.js
@@ -0,0 +1,50 @@
+import React,{PureComponent,Fragment} from 'react';
+import {Input,Icon} from 'antd'
+import hoc from './dailihoc'
+class Cat extends React.Component {
+    render() {
+      const mouse = this.props.mouse;
+      console.log(mouse,'--->mouse')
+      return (
+        <Icon type="right-circle" style={{ position: 'absolute', left: mouse.x, top: mouse.y }}/>
+      );
+    }
+  }
+  
+  class Mouse extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleMouseMove = this.handleMouseMove.bind(this);
+      this.state = { x: 0, y: 0 };
+    }
+  
+    handleMouseMove(event) {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      });
+    }
+  
+    render() {
+      return (
+        <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
+            dsaf
+          {this.props.render(this.state)}
+        </div>
+      );
+    }
+  }
+  
+  class MouseTracker extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>移动鼠标!</h1>
+          <Mouse render={mouse => (
+            <Cat mouse={mouse} />
+          )}/>
+        </div>
+      );
+    }
+  }
+export default MouseTracker;
--- src/pages/Hoc/index.js
+++ src/pages/Hoc/index.js
@@ -1,15 +1,18 @@
 import React,{PureComponent,Fragment} from 'react'; 
-import A from './components/a'
-import B from './components/b'
-import C from './components/c'
+import { Divider } from 'antd'
+import { A,B,C,RenderProp } from './components'
 
  class Hoc extends PureComponent{
     render(){
         return(
             <Fragment>
                 <A name='小A' age={18}/>
+                <Divider/>
                 <B/>
+                <Divider/>
                 <C/>
+                <Divider>render prop</Divider>
+                <RenderProp/>
             </Fragment>
         )
     }
