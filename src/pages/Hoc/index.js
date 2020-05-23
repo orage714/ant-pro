e9cfc2d201de7f7817d523b14e14dfc14ce47832
smallorange <957644070@qq.com>
2020-05-23 22:34:14 +0800

    :sparkles:添加csv

--- src/pages/Hoc/index.js
+++ src/pages/Hoc/index.js
@@ -1,11 +1,22 @@
 import React,{PureComponent,Fragment} from 'react'; 
-import { Divider } from 'antd'
+import { Divider, Button } from 'antd'
 import { A,B,C,RenderProp } from './components'
 
  class Hoc extends PureComponent{
+    exportCsv=(data, name)=> {
+        // “\ufeff” BOM头
+        var uri = 'data:text/csv;charset=utf-8,\ufeff' + encodeURIComponent(data);
+        var downloadLink = document.createElement("a");
+        downloadLink.href = uri;
+        downloadLink.download = (name+".csv")||"temp.csv";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    }
     render(){
         return(
             <Fragment>
+                <Button onClick={()=>this.exportCsv("dapianzi,卡夫卡,真理君,宿敌\n黄德菊,都是湿的,未完成", "王路飞")}>导出CSV文件</Button>
                 <A name='小A' age={18}/>
                 <Divider/>
                 <B/>
