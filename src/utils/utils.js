109d1f1a1cfc15bbd787ad5d2874632d93afb4fc
smallorange <957644070@qq.com>
2020-06-02 21:12:38 +0800

    :sparkles:add 获取时间段

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,7 +5,7 @@ export function isUrl(path) {
   return reg.test(path);
 }
 
-// 获取路由锚地
+// 获取路由锚点
 export  function scrollToAnchor(){
   if (window.location.hash) {
     const href = window.location.hash.slice(1, window.location.hash.length);
@@ -14,3 +14,21 @@ export  function scrollToAnchor(){
     }
   }
 };
+
+// 当前时间是否在某个时间段之间
+export function timeRange(beginTime, endTime,split) {
+  let strb = beginTime.split (split),stre = endTime.split (split);
+  if (strb.length != 2||stre.length != 2) return false;
+  let b = new Date ();
+  let e = new Date ();
+  let n = new Date ();
+  b.setHours (strb[0]);
+  b.setMinutes (strb[1]);
+  e.setHours (stre[0]);
+  e.setMinutes (stre[1]);
+
+  if (n.getTime () - b.getTime () > 0 && n.getTime () - e.getTime () < 0) {
+      return true;
+  }
+  return false;
+}
